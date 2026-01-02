2a1177958bcab0b9ae16e344a93b904274f294f8
Warapob-AI <paintseason158@gmail.com>
2026-01-02 20:03:27 +0700

    Dev: solving feature calculate

--- src/calculate.py
+++ src/calculate.py
@@ -1,4 +1,4 @@
-def check_int(num1, num2): 
+def add_plus(num1, num2):
     if isinstance(num1, int): 
         num1 = float(num1) 
     
@@ -7,12 +7,17 @@ def check_int(num1, num2):
 
     if not isinstance(num1, float) or not isinstance(num2, float):
         return "Error: Both arguments must be float"
-
-def add_plus(num1, num2):
-    check_int(num1, num2)
     
     return num1 + num2
 
 def add_negative(num1, num2): 
-    check_int(num1, num2)
+    if isinstance(num1, int): 
+        num1 = float(num1) 
+    
+    if isinstance(num2, int): 
+        num2 = float(num2)
+
+    if not isinstance(num1, float) or not isinstance(num2, float):
+        return "Error: Both arguments must be float"
+    
     return num1 - num2
